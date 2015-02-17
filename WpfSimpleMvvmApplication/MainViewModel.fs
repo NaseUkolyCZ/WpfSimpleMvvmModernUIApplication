4ce03edb2f2942d5ca231a6ad5cdab7758423ce7
davidpodhola <david.podhola@naseukoly.cz>
2015-02-17 22:56:46 +0100

    buildable again

--- WpfSimpleMvvmApplication/MainViewModel.fs
+++ WpfSimpleMvvmApplication/MainViewModel.fs
@@ -5,6 +5,8 @@ open System.Windows
 open System.Windows.Input
 
 open FsXaml
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
 
 type MainViewModel() as self = 
     inherit ViewModelBase()
@@ -13,14 +15,16 @@ type MainViewModel() as self =
     let lastName = self.Factory.Backing(<@ self.LastName @>, "")
     let hasValue str = not(System.String.IsNullOrWhiteSpace(str))
     let okCommand = 
-        self.Factory.CommandSyncCheckedParam(
+        self.Factory.CommandSyncParamChecked(
             (fun param -> MessageBox.Show(sprintf "Hello, %s" param) |> ignore), 
             (fun param -> hasValue self.FirstName && hasValue self.LastName), 
             [ <@ self.FirstName @> ; <@ self.LastName @> ])   // Or could be: [ <@ self.FullName @> ])
 
+(* 
     do
         // Add in property dependencies
         self.Factory.SetPropertyDependencies(<@ self.FullName @>, [ <@ self.FirstName @> ; <@ self.LastName @> ])
+*)
     
     member x.FirstName with get() = firstName.Value and set value = firstName.Value <- value
     member x.LastName with get() = lastName.Value and set value = lastName.Value <- value
